9ffbc18ff51b9ad8a79786f1cf306b9632af444f
binitbanerjee <binitbanerjee@gmail.com>
2019-11-23 16:45:41 -0500

    updating the table names.

--- lib/server.ex
+++ lib/server.ex
@@ -7,6 +7,9 @@ defmodule Server do
 
   def init(_args) do
     :ets.new(:users,[:set, :public, :named_table])
+    :ets.new(:following,[:set, :public, :named_table])
+    :ets.new(:subscription,[:set, :public, :named_table])
+    :ets.new(:tweets,[:set, :public, :named_table])
     {:ok,%{}}
   end
 
@@ -20,12 +23,41 @@ defmodule Server do
     {:reply,from,state}
   end
 
+  def handle_call({:subscribe_to_user, target_user_id, source_user_id}) do
+    if (isUserValid(target_user_id) == true and isUserValid(source_user_id) == true) do
+      ## add the source user as a subscriber to target
+      [{_, existing_subscription }] = :ets.lookup(:subscription, target_user_id)
+      existing_subscription = [source_user_id | existing_subscription]
+      :ets.insert(:subscription,{source_user_id, existing_subscription})
+
+      ## add the target as a user whom the source is following.
+      [{_, existing_users_following }] = :ets.lookup(:following, source_user_id)
+      existing_users_following = [target_user_id | existing_users_following]
+      :ets.insert(:subscription,{source_user_id, existing_users_following})
+
+
+    end
+  end
+
   defp register_user(pid,user_id) do
-    :ets.insert(:users, {user_id, [pid,[],[],[]]})
+    :ets.insert(:users, {user_id, pid})
+    :ets.insert(:following, {user_id, []})
+    :ets.insert(:subscription, {user_id, []})
+    :ets.insert(:tweets, {user_id, []})
   end
 
   defp delete_user(user_id) do
-    :ets.insert(:users,{user_id,[-1,[],[],[]]})
+    :ets.insert(:users,{user_id,-1})
+  end
+
+  defp isUserValid(user_id) do
+    resp =3
+    if ( :ets.lookup(:users, user_id) == [] && :ets.lookup(:following, user_id) == [] and :ets.lookup(:tweets, user_id) == [] and :ets.lookup(:subscription, user_id) == []) do
+        false
+    else
+        true
+    end
+    resp
   end
 
 end
